dbdcac2d7b4ac6f9826e49ca095c0bd69744bae9
Joel Rivera <rivera@joel.mx>
2019-07-24 20:35:37 -0500

    Add the __version__ attribute. Release 0.1.1.

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@
 CHANGELOG
 =========
 
+0.1.1
+-----
+
+* Add the ``__version__`` attribute in the main package ``cherrypy_ask_sdk``.
+* Small changes in the package metadata (the license wasn't specified in the wheel).
+
 0.1
 ---
 
--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ config = read_configuration("setup.cfg")
 setup_params = {}
 setup_params = dict(setup_params, **config['metadata'])
 setup_params = dict(setup_params, **config['options'])
+# at this point there is not a simpler way to load a requires file
+# directly from the setup.cfg file
+setup_params['install_requires'] = [
+    line.strip() for line in open("requires.txt").read().splitlines()
+]
 setuptools.setup(**setup_params)
